f2407afa75fc4e2faf6170d1b18b75d001e7f1b2
thedevluffy <thedevluffy@gmail.com>
2021-12-07 20:30:27 +0900

    feat : add disable features to DefaultButton.tsx for admin

--- tecobrary-user-client/src/admin/components/DefaultButton.tsx
+++ tecobrary-user-client/src/admin/components/DefaultButton.tsx
@@ -1,47 +1,74 @@
-import React, {ReactElement} from "react";
+import React, {ReactElement, useEffect, useState} from "react";
 import styled from "styled-components";
 import {useHistory} from "react-router-dom";
 
 export interface Props {
   text: string
+  disabled?: boolean
   to?: string
   onClick?: () => void
 }
 
+interface DisableableProps {
+  disabled: boolean
+}
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<DisableableProps>`
   border-radius: 4px;
   width: fit-content;
   height: 2rem;
-  background-color: rgba(0, 0, 0, 0.1);
-  border: 1px solid black;
+  background-color: rgba(43, 43, 43);
+  border: 1px solid rgba(43, 43, 43);
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  cursor: pointer;
   margin: 0 4px;
+  cursor: ${props => props.disabled ? "disabled" : "pointer"};
 `
 
-const Text = styled.div`
+const Text = styled.div<DisableableProps>`
   margin: 1rem;
   font-weight: bold;
   font-size: small;
-  color: #3498db;
+  color: ${props => props.disabled ? "rgb(90, 90, 90)" : "rgb(255, 255, 255)"};
+  cursor: default;
   height: fit-content;
 `
 
-function DefaultButton({text, to, onClick}: Props): ReactElement {
+function DefaultButton({text, disabled, to, onClick}: Props): ReactElement {
+
+  const [buttonDisabled, setButtonDisabled] = useState(disabled)
+
+  useEffect(() => {
+    if (disabled == undefined) {
+      return
+    }
+    setButtonDisabled(!disabled)
+  }, [disabled])
 
   const history = useHistory()
 
-  const onClickTo = () => {
+  const onClickAction = () => {
+    if (buttonDisabled) {
+      doNothingOnClick()
+      return
+    }
+    if (onClick) {
+      onClick()
+      return;
+    }
     history.push(`${to}`)
   }
 
+  const doNothingOnClick = () => {
+    console.debug("아무것도 하지 않음")
+  }
+
   return (
-    <Wrapper onClick={onClick ? onClick : onClickTo}>
-      <Text>
+    <Wrapper className={"noselect"} onClick={onClickAction}
+             disabled={buttonDisabled ? buttonDisabled : false}>
+      <Text disabled={buttonDisabled ? buttonDisabled : false}>
         {text}
       </Text>
     </Wrapper>
