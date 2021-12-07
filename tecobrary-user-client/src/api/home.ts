a5af45a4724d6e90550cbf0ba23fc37a69bbbd94
thedevluffy <thedevluffy@gmail.com>
2021-12-07 20:57:43 +0900

    edit : replace mock data for advertising

--- tecobrary-user-client/src/api/home.ts
+++ tecobrary-user-client/src/api/home.ts
@@ -3,21 +3,21 @@ import {Category, InterestedBooks, NewBook} from "../interfaces";
 export const getNewArrivalBooks: NewBook[] = [
   {
     id: 1,
-    imageUrl: "http://image.kyobobook.co.kr/images/book/xlarge/727/x9788960883727.jpg",
-    title: "오늘도 개발자가 안 된다고 말했다",
-    author: "김중철, 김수지",
+    imageUrl: "http://image.kyobobook.co.kr/images/book/large/758/l9791158392758.jpg",
+    title: "만들면서 배우는 클린 아키텍처",
+    author: "톰 홈버그, 박소은(옮긴이)",
   },
   {
     id: 2,
-    imageUrl: "http://image.kyobobook.co.kr/images/book/xlarge/766/x9788998139766.jpg",
-    title: "객체지향의 사실과 오해",
-    author: "조영호",
+    imageUrl: "http://image.kyobobook.co.kr/images/book/large/864/l9791158392864.jpg",
+    title: "따라하며 배우는 도커와 CI 환경",
+    author: "안재원",
   },
   {
     id: 3,
-    imageUrl: "http://image.kyobobook.co.kr/images/book/xlarge/670/x9788966262670.jpg",
-    title: "실전 리액트 프로그래밍 리액트 훅부터 Next.js까지 개정판",
-    author: "이재승",
+    imageUrl: "http://image.kyobobook.co.kr/images/book/large/172/l9788966263172.jpg",
+    title: "밑바닥부터 만드는 컴파일러 IN GO",
+    author: "토르슈텐 발",
   },
   {
     id: 4,
