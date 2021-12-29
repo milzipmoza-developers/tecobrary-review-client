02ff69e0569c643d9725f5a5cb278f9dc077c2ad
thedevluffy <thedevluffy@gmail.com>
2021-12-29 20:14:26 +0900

    feat: add configuration for theme color

--- src/admin/components/AdminFrame.tsx
+++ src/admin/components/AdminFrame.tsx
@@ -2,9 +2,10 @@ import {ReactElement, ReactNode} from "react";
 import {AdminHeader} from "./AdminHeader";
 import DefaultButton from "./DefaultButton";
 import styled from "styled-components";
-import {Alert, Snackbar} from "@mui/material";
+import {Alert, Snackbar, ThemeProvider} from "@mui/material";
 import {useRecoilState} from "recoil";
 import {adminAlertStatus} from "../status/AdminAlertStatus";
+import {theme} from "../config/color";
 
 interface Props {
   children: ReactNode
@@ -25,27 +26,29 @@ export const AdminFrame = ({children}: Props): ReactElement => {
 
   return (
     <>
-      <AdminHeader>
-        <DefaultButton text="어드민 홈" to={"/admin"}/>
-        <DefaultButton text="어드민 도서" to={"/admin/books"}/>
-        <DefaultButton text="어드민 카테고리" to={"/admin/categories"}/>
-        <DefaultButton text="어드민 태그" to={"/admin/tags"}/>
-        <DefaultButton text="어드민 마크" to={"/admin/marks"}/>
-        <DefaultButton text="어드민 신간도서" to={"/admin/new-arrivals"}/>
-      </AdminHeader>
-      <AdminBody>
-        {children}
-      </AdminBody>
-      <Snackbar
-        open={alertStatus.open}
-        autoHideDuration={alertStatus.duration ? alertStatus.duration : 2000}
-        anchorOrigin={{vertical: "bottom", horizontal: "right"}}
-        onClose={handleAlertClose}
-      >
-        <Alert onClose={handleAlertClose} severity={alertStatus.severity}>
-          {alertStatus.message}
-        </Alert>
-      </Snackbar>
+      <ThemeProvider theme={theme}>
+        <AdminHeader>
+          <DefaultButton text="어드민 홈" to={"/admin"}/>
+          <DefaultButton text="어드민 도서" to={"/admin/books"}/>
+          <DefaultButton text="어드민 카테고리" to={"/admin/categories"}/>
+          <DefaultButton text="어드민 태그" to={"/admin/tags"}/>
+          <DefaultButton text="어드민 마크" to={"/admin/marks"}/>
+          <DefaultButton text="어드민 신간도서" to={"/admin/new-arrivals"}/>
+        </AdminHeader>
+        <AdminBody>
+          {children}
+        </AdminBody>
+        <Snackbar
+          open={alertStatus.open}
+          autoHideDuration={alertStatus.duration ? alertStatus.duration : 2000}
+          anchorOrigin={{vertical: "bottom", horizontal: "right"}}
+          onClose={handleAlertClose}
+        >
+          <Alert onClose={handleAlertClose} severity={alertStatus.severity}>
+            {alertStatus.message}
+          </Alert>
+        </Snackbar>
+      </ThemeProvider>
     </>
   )
 }
---
+++ src/admin/config/color.ts
@@ -0,0 +1,12 @@
+import {createTheme} from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    secondary: {
+      light: 'rgb(85, 85, 85)',
+      main: 'rgb(60, 63, 65)',
+      dark: 'rgb(49, 51, 53)',
+      contrastText: '#FFF',
+    },
+  },
+});
